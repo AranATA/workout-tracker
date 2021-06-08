b8cddd005736e2f03b171747a95408e91bd094c7
Ghibli <aran.t.ali@gmail.com>
2021-06-08 10:46:36 -0400

    model files created

---
+++ models/index.js
@@ -0,0 +1,8 @@
+const Workout = require('./workout.js')
+
+module.exports =  Workout;
+
+// OR:
+// module.exports = {
+//   Workout: require('./workout')
+// };
---
+++ models/workout.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+
+// Mongoose Schema
+const Schema = mongoose.Schema;
+
+// Create new workout schema
+const WorkoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now()
+  },
+  exercises: [
+    {
+      name : {
+        type : String,
+        trim : true,
+        required : "Enter Exercise Name"
+      },
+      type : {
+        type: String,
+        trim : true,
+        required : "Enter Exercise Type"
+      },
+      distance : {
+        type : Number
+      },
+      duration : {
+        type : Number,
+        required : "Enter Exercise Duration"
+      },
+      weight: {
+        type : Number
+      },
+      sets: {
+        type : Number
+      },
+      reps: {
+        type : Number
+      }
+    }
+  ]
+});
+
+
+
+
+
+// Create mongoose model 'workout' and apply workout schema to that model
+const Workout = mongoose.model("workout", WorkoutSchema);
+
+// Export workout model
+module.exports = Workout;
