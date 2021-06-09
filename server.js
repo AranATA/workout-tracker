bcf97f2e4618e258dee57fc0c5fe5d22ffefa396
Ghibli <aran.t.ali@gmail.com>
2021-06-08 22:46:42 -0400

    deploying to Heroku

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use(express.json());
 // make public folder static 
 app.use(express.static("public"));
 
+// code added for Mongo Atlas deployment
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/workout",
   { 
